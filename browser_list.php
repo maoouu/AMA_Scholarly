e42ac7b466a05e13742557cfb3ca7a26f125f577
Dale Reyes <33371589+maoouu@users.noreply.github.com>
2019-03-31 22:13:09 +0800

    Create browser_list.php

---
+++ browser_list.php
@@ -0,0 +1,23 @@
+<?php
+
+    session_start();
+    $sql = "";
+    //echo $_SESSION["title"] . "<br>";
+    //echo $_SESSION["category"] . "<br>";
+    //echo $_SESSION["author"] . "<br>";
+
+    if ($_SESSION["title"]) {
+        # code...
+    }
+
+    if ($_SESSION["category"]) {
+        # code...
+    }
+
+    if ($_SESSION["author"]) {
+        # code...
+    }
+
+    //to be continued
+
+?>
