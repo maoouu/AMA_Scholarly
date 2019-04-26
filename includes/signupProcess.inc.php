7192f20a6a8714451618e8f8ea601b936e5167f1
Dale Reyes <33371589+maoouu@users.noreply.github.com>
2019-04-26 21:43:57 +0800

    added legitimate USN verification

--- includes/signupProcess.inc.php
+++ includes/signupProcess.inc.php
@@ -1,4 +1,26 @@
 <?php
+
+  function globalUSN($usn) { //this functions looks for the legit USN
+    require 'dbh.inc.php';
+    $sql = "SELECT * FROM usn_collection WHERE USN = ?";
+    $stmt = mysqli_stmt_init($conn);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      echo "SQL Statement Function Error";
+    } else {
+      mysqli_stmt_bind_param($stmt, "s", $usn);
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+
+      if ($result) {
+        return true;
+      } else {
+        return false;
+      }
+      
+    }
+  }
+
   function checkUSN($usn) { //this function checks for duplicates
     require 'dbh.inc.php';
     $sql = "SELECT * FROM user_tbl WHERE user_USN = ?;";
@@ -33,37 +55,42 @@
     $password = sha1($password);
     $confirmPassword = sha1($confirmPassword);
 
-    //Checking if USN exists
-    $checkUSN = checkUSN($usn);
+    //Checking if USN exists in a USN database
+    $globalUSN = globalUSN($usn);
 
-    if ($checkUSN >= 1) {
-      header('Location: ../signup.php?duplicate');
+    if ($globalUSN == false) {
+      header('Location: ../signup.php?id=false');
     } else {
-      // Validating password with confirm Password
-      // And doing a prepared statement to prevent SQL Injections
-      if ($password == $confirmPassword) {
-        $sql = "INSERT INTO user_tbl (user_fullname, user_USN, user_email, user_password)
-        VALUES (?, ?, ?, ?);";
+      //Checking if USN exists in a login
+      $checkUSN = checkUSN($usn);
 
-        $stmt = mysqli_stmt_init($conn);
-
-        if (!mysqli_stmt_prepare($stmt, $sql)) {
-          echo "<script>alert('An error occurred: SQL Statement Failed');
-          location.href='../index.php'</script>";
-        } else {
-          mysqli_stmt_bind_param($stmt, "ssss", $fullname, $usn, $email, $password);
-          mysqli_stmt_execute($stmt);
-          include 'picture_upload.inc.php';
-          echo "<script>alert('New user added to the database!');
-          location.href='../index.php?success'</script>";
-        }
-
+      if ($checkUSN >= 1) {
+        header('Location: ../signup.php?duplicate');
       } else {
-        echo 'Signup failed <a href="../signup.php">Signup</a>';
+        // Validating password with confirm Password
+        // And doing a prepared statement to prevent SQL Injections
+        if ($password == $confirmPassword) {
+          $sql = "INSERT INTO user_tbl (user_fullname, user_USN, user_email, user_password)
+          VALUES (?, ?, ?, ?);";
+
+          $stmt = mysqli_stmt_init($conn);
+
+          if (!mysqli_stmt_prepare($stmt, $sql)) {
+            echo "<script>alert('An error occurred: SQL Statement Failed');
+            location.href='../index.php'</script>";
+          } else {
+            mysqli_stmt_bind_param($stmt, "ssss", $fullname, $usn, $email, $password);
+            mysqli_stmt_execute($stmt);
+            include 'picture_upload.inc.php';
+            echo "<script>alert('New user added to the database!');
+            location.href='../index.php?success';</script>";
+          }
+
+        } else {
+          echo 'Signup failed <a href="../signup.php">Signup</a>';
+        }
       }
     }
-
-
   } else {
     header('Location: ../signup.php');
   }
