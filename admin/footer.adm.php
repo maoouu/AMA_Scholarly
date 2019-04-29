214bdfe5019b28029cece26b14f5b624a18e2d6a
Dale Reyes <33371589+maoouu@users.noreply.github.com>
2019-04-29 11:15:45 +0800

    Create footer.adm.php

---
+++ admin/footer.adm.php
@@ -0,0 +1,12 @@
+<footer class="footer" style="background-color:#FFF">
+<div class="container">
+    <div class="content has-text-centered">
+        <a href="https://bulma.io">
+            <img src="../img/made-with-bulma--semiblack.png" alt="Made with Bulma" width="128" height="24">
+        </a> <br>
+        <p class="subtitle is-6">This page is <strong>open source</strong>.
+        Want to help with the development of the page? <br>
+        <a href="https://github.com/maoouu/AMA_Scholarly" target="_blank" style="text-decoration:underline; color:dimgray">Improve this page on github</a>. <br></p>
+    </div>
+</div>
+</footer>
