ce9bceb493df713b88ba9771b809a1df4d82d5a0
Dale Reyes <33371589+maoouu@users.noreply.github.com>
2020-02-12 09:33:18 +0800

    fixed preview_body.php having profile.php

--- preview_body.php
+++ preview_body.php
@@ -1,46 +1,108 @@
-<div class="container">
-  <section class="hero is-small">
-    <div class="hero-body">
-      <div class="column">
-        <figure class="image  is-128x128">
-          <img class="is-rounded" src="<?php 
-          $imgFile = "profile-pic/" . $_SESSION['fullname'] . ".jpg";
-          $defaultFile = "profile-pic/default.png";
-          if (file_exists($imgFile)) {
-            echo $imgFile;
-          } else {
-            echo $defaultFile;
-          }
-          ?>" alt="profile picture">
-        </figure>
+<div class="section">
+  <div class="container">
+    <div class="columns level">
+
+      <div class="column level-left">
+        <!-- Paper Review -->
+        <div class="card">
+
+          <div class="card-content">
+            <!-- Title -->
+            <p class="title is-6"><?php echo $_SESSION["preview_title"]; ?></p>
+            <hr><!-- Insert php document title -->
+            <!-- Placeholder Thumbnail -->
+            <figure class="image is-1by1">
+              <a href="pdfs/<?php echo $_SESSION["preview_file"] . "#toolbar=0&navpanes=0"; ?>"><img src="img/100x150.png"></a> <!-- Add document location soon -->
+            </figure> <br>
+
+          </div>
+
+        </div>
       </div>
-      <div class="column">
-        <h1 class="title is-4" style="text-align:left;"><?php echo $_SESSION['fullname']; ?></h1>
-        <p class="subtitle is-6"style="text-align:left;">Joined <?php $dateTime = new DateTime($_SESSION['date']); echo date_format($dateTime, "M. d, Y") ?></p>
+
+      <div class="column level-right">
+        <!-- Page-Info -->
+        <div class="card">
+          <div class="card-content">
+            <!-- Details -->
+            <p class="title is-6 has-text-left">About</p>
+            <hr>
+            <p class="is-6 has-text-left">Presented By:
+              <?php
+              include 'includes/dbh.inc.php';
+              $id = $_SESSION['preview_ID'];
+              $sql = "SELECT db_authorFirstName1, db_authorLastName1,
+                        db_authorFirstName2, db_authorLastName2,
+                        db_authorFirstName3, db_authorLastName3,
+                        db_authorFirstName4, db_authorLastName4,
+                        db_authorFirstName5, db_authorLastName5,
+                        db_authorFirstName6, db_authorLastName6, db_citationInput, db_views FROM Metadata WHERE db_ID = '$id'";
+              $query = mysqli_query($conn, $sql);
+
+              while ($row = mysqli_fetch_array($query)) {
+                $views = $row['db_views'];
+                $citation = $row['db_citationInput'];
+                switch ($_SESSION["author_quantity"]) {
+                  case '1':
+                    echo $row['db_authorFirstName1'] . " " . $row['db_authorLastName1'];
+                    break;
+                  case '2':
+                    echo $row['db_authorFirstName1'] . " " . $row['db_authorLastName1'] . " and " .
+                      $row['db_authorFirstName2'] . " " . $row['db_authorLastName2'];
+                    break;
+                  case '3':
+                    echo $row['db_authorFirstName1'] . " " . $row['db_authorLastName1'] . ", " .
+                      $row['db_authorFirstName2'] . " " . $row['db_authorLastName2'] . " and " .
+                      $row['db_authorFirstName3'] . " " . $row['db_authorLastName3'];
+                    break;
+                  case '4':
+                    echo $row['db_authorFirstName1'] . " " . $row['db_authorLastName1'] . ", " .
+                      $row['db_authorFirstName2'] . " " . $row['db_authorLastName2'] . ", " .
+                      $row['db_authorFirstName3'] . " " . $row['db_authorLastName3'] . " and " .
+                      $row['db_authorFirstName4'] . " " . $row['db_authorLastName4'];
+                    break;
+                  case '5':
+                    echo $row['db_authorFirstName1'] . " " . $row['db_authorLastName1'] . ", " .
+                      $row['db_authorFirstName2'] . " " . $row['db_authorLastName2'] . ", " .
+                      $row['db_authorFirstName3'] . " " . $row['db_authorLastName3'] . ", " .
+                      $row['db_authorFirstName4'] . " " . $row['db_authorLastName4'] . " and " .
+                      $row['db_authorFirstName5'] . " " . $row['db_authorLastName5'];
+                    break;
+                  case '6':
+                    echo $row['db_authorFirstName1'] . " " . $row['db_authorLastName1'] . ", " .
+                      $row['db_authorFirstName2'] . " " . $row['db_authorLastName2'] . ", " .
+                      $row['db_authorFirstName3'] . " " . $row['db_authorLastName3'] . ", " .
+                      $row['db_authorFirstName4'] . " " . $row['db_authorLastName4'] . ", " .
+                      $row['db_authorFirstName5'] . " " . $row['db_authorLastName5'] . " and " .
+                      $row['db_authorFirstName6'] . " " . $row['db_authorLastName6'];
+                    break;
+
+                  default:
+                    echo "N/A";
+                    break;
+                }
+              }
+
+              ?>
+            </p>
+            <p class="is-6 has-text-left">Category: <?php echo $_SESSION["preview_category"]; ?></p>
+            <p class="is-6 has-text-left">Date Published: <?php $dateTime = new DateTime($_SESSION["preview_date_created"]);
+                                                          echo date_format($dateTime, "M. d, Y"); ?></p>
+            <hr>
+            <div class="field is-grouped">
+              <p class="is-6 has-text-left" style="padding-right:5px;">Citation:</p>
+              <input class="input is-small is-rounded is-disabled" type="text" id="copyText" value="<?php echo $citation; ?>" readonly> <!-- Sa ngayon, di pa magawa ng automatic, kase di pa nahihiwalay ang first name, at last name, etc. -->
+              <button class="button is-small" onclick="copyText()">Copy Text</button>
+            </div>
+            <br>
+            <?php if ($views == 1) {
+              echo $views . " view";
+            } else {
+              echo $views . " views";
+            } ?>
+          </div>
+        </div>
       </div>
     </div>
-  </section>
-
-  <section class="section">
-  <div class="container">
-  <div class="columns">
-      <!-- Side menu -->
-  <div class="column is-2 is-sidebar-menu">
-    <aside class="menu">
-      <p class="menu-label">User Menu</p>
-      <ul class="menu-list">
-        <li><a class="is-active">Settings</a></li>
-      </ul>
-    </aside>
   </div>
-
-  <!-- Main content -->
-  <div class="column is-main-content" style="background-color:white;">
-    
-  </div>
-  
-  </div>
-  </div>
-  </section>
-
 </div>
